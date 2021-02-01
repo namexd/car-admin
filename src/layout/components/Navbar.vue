eb1890ffff1031ca0179596665e3dce40775c528
namexd <namexd@163.com>
2021-02-01 21:49:35 +0800

    退出登录

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -8,18 +8,19 @@
       <el-dropdown class="avatar-container" trigger="click">
         <div class="avatar-wrapper">
 <!--          <img :src="avatar+'?imageView2/1/w/80/h/80'" class="user-avatar">-->
-          <i class="el-icon-caret-bottom" />
+          <span style="display:block;cursor: pointer">退出</span>
+<!--          <i class="el-icon-caret-bottom" />-->
         </div>
-        <el-dropdown-menu slot="dropdown" class="user-dropdown">
-          <router-link to="/">
-            <el-dropdown-item>
-              首页
-            </el-dropdown-item>
-          </router-link>
-          <el-dropdown-item divided @click.native="logout">
-            <span style="display:block;">退出</span>
-          </el-dropdown-item>
-        </el-dropdown-menu>
+<!--        <el-dropdown-menu slot="dropdown" class="user-dropdown">-->
+<!--          <router-link to="/">-->
+<!--            <el-dropdown-item>-->
+<!--              首页-->
+<!--            </el-dropdown-item>-->
+<!--          </router-link>-->
+<!--          <el-dropdown-item divided @click.native="logout">-->
+<!--            <span style="display:block;">退出</span>-->
+<!--          </el-dropdown-item>-->
+<!--        </el-dropdown-menu>-->
       </el-dropdown>
     </div>
   </div>
